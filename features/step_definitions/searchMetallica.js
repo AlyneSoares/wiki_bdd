1ee8ff6eec083502cd5eb6238767e4e74b8cbfd5
Alyne MR Soares <35814001+AlyneSoares@users.noreply.github.com>
2019-01-07 18:15:20 -0200

    Delete searchMetallica.js

--- features/step_definitions/searchMetallica.js
+++
@@ -1,4 +0,0 @@
-const { Given, When, Then } = require('cucumber');
-const env = require('../support/environment').getInstance();
-const hooks = require('../support/hooks');
-const { selectors } = require('../support/selectors');
