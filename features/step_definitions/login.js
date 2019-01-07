0e3865de9127c2bfe73f21ecb1958851cb9ac679
Alyne MR Soares <35814001+AlyneSoares@users.noreply.github.com>
2019-01-07 18:18:20 -0200

    adapting to model

--- features/step_definitions/login.js
+++ features/step_definitions/login.js
@@ -4,8 +4,8 @@ const hooks = require('../support/hooks');
 const { selectors } = require('../support/selectors');
 
 //Login Step by Step
-Given('I open the OCC page', async function () {
-    this.consoleToReport('Given', 'I open the OCC page');
+Given('I open the Google page', async function () {
+    this.consoleToReport('Given', 'I open the Google page');
     await testController.navigateTo(env.getUrl()).wait(10000);
 });
 When('I type my user email', async function () {
@@ -15,8 +15,4 @@ When('I type my user email', async function () {
 When('I type my password', async function () {
     this.consoleToReport("When", 'I type my password');
     await testController.typeText(selectors.passwordFormField(), env.getPassword());
-});
-When('I type my password in the {string}', async function (keySelector) {
-  this.consoleToReport('Then', 'I type my password in the {string}', keySelector);
-  await testController.typeText(selectors[keySelector](), env.getPassword());
 });
